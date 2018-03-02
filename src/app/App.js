a65b10c15a12ff1c0979877f141fc06e15ba64a7
Linus Falck-Ytter <lifayt@gmail.com>
2018-03-02 16:52:47 -0500

    Initial Commit

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+        </div>
+        <div className="container">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
